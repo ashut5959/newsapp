f5b1fc56ecc67fc14976e5c37f9656458e6d5a97
ashut5959 <ashutoshpal59@gmail.com>
2022-10-26 20:58:07 +0530

    function(9:00Pm)
    updated,,
    spinner

---
+++ src/components/Spinner.js
@@ -0,0 +1,12 @@
+import React from "react";
+import loading from './img/loader.gif'
+
+const Spinner = () => {
+    return (
+        <div className="text-center">
+            <img className="md-3" src={loading} alt="loading" />
+        </div>
+    )
+}
+
+export default Spinner
